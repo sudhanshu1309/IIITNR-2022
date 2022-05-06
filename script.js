d8e78410e957572722944ebe979821b8cf75ac2c
Sudhanshu Tripathi <sudhanshu@Sudhanshus-MacBook-Air.local>
2022-05-06 18:54:42 +0530

    changed alignment

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const surpriseAt = "Fri May 06 2022 19:00:00 GMT+0530";
+const surpriseAt = "Fri May 06 2022 20:00:00 GMT+0530";
 
 function countdown() {
     const newYearsDate = new Date(surpriseAt);
